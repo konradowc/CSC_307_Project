202ce66acc3abd88e7d2712f21fef16b5da67061
Sriya Adunur <sadunur@calpoly.edu>
2025-04-29 14:12:39 -0700

    Added createBlog component

--- packages/react-frontend/src/App.jsx
+++ packages/react-frontend/src/App.jsx
@@ -1,9 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import CreatePost from "./CreatePost"; 
 import "./App.css";
 
 function App() {
+  return (
+    <div>
+       <CreatePost />
+    </div>
+  );
+}
+
+/*function App() {
   const [count, setCount] = useState(0);
 
   return (
@@ -38,6 +47,6 @@ function App() {
       </p>
     </>
   );
-}
+}*/
 
 export default App;
