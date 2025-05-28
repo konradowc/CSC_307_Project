f3cb8292451f51b615b37143c8d29b8835adca1e
Konrad <>
2025-05-28 12:17:19 -0700

    testing

--- packages/react-frontend/src/pages/Profile.jsx
+++ packages/react-frontend/src/pages/Profile.jsx
@@ -43,8 +43,11 @@ const Profile = () => {
   useEffect(() => {
     if (!token) return;
 
+    const vite_url = `${import.meta.env.VITE_BACKEND_URL}`;
+    console.log("vite_backend_url: " + vite_url);
+
     // Fetch user info, then fetch posts for that user
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/users`, {
+    fetch(url + `/users`, {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
